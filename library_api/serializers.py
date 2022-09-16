1cd1f34b198196ffad17bb5bf4d887b35b6204f0
Thomas Morris <tomchrismorris@yahoo.com>
2022-09-16 15:57:13 -0400

    added hyperlinks to serializers

--- library_api/serializers.py
+++ library_api/serializers.py
@@ -2,26 +2,26 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import CustomUser, Book, Track, Note
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'publication_date', 'genre', 'featured',)
+        fields = ('url', 'id', 'title', 'author', 'publication_date', 'genre', 'featured',)
 
 
-class TrackSerializer(serializers.ModelSerializer):
+class TrackSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.SlugRelatedField(slug_field="username", read_only=True)
     book = serializers.SlugRelatedField(slug_field="title", read_only=True)
 
     class Meta:
         model = Track
-        fields = ('id', 'user', 'book', 'status',)
+        fields = ('url', 'id', 'user', 'book', 'status',)
 
 
-class NoteSerializer(serializers.ModelSerializer):
+class NoteSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.SlugRelatedField(slug_field="username", read_only=True)
     book = serializers.SlugRelatedField(slug_field="title", read_only=True)
     
     class Meta:
         model = Note
-        fields = ('id', 'user', 'book', 'created_at', 'note', 'private', 'page',)
+        fields = ('url', 'id', 'user', 'book', 'created_at', 'note', 'private', 'page',)
